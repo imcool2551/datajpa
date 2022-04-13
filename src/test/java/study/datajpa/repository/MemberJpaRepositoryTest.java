4d0a36b8f1e1cf833a1c6ca105a7e10900c5ab00
imcool2551 <imcool2551@gmail.com>
2022-04-13 20:53:19 +0900

    settings

---
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,27 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    void testMember() {
+        Member member = new Member("memberA");
+
+        memberJpaRepository.save(member);
+        Member findMember = memberJpaRepository.find(member.getId());
+
+        assertThat(findMember).isEqualTo(member);
+    }
+}
